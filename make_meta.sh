bf69b5cdd0bdb457f9bdf4f000599836b5537136
eddycolloton <eddy.colloton@gmail.com>
2020-10-01 15:50:49 -0400

    Update make_meta.sh
    Took out processes that are now in the make_dirs.sh script.
    
    Changed the case statements to record a variable based on the user's choice, instead of running the command immediately after the user input. 
    
    Added if statements to read the assigned variable and run the commands after all of the case statements.

--- make_meta.sh
+++ make_meta.sh
@@ -1,86 +1,14 @@
 #!/bin/bash
 
+#I'm trying to start breaking up the processes in the script, so this just the metadata tools that are run on the files
+#It requires inputs gathered from the make_dirs_config.sh though, so it's not really a stand a lone script. make_dirs has to be run first.
+#I should start this script with an if statement that checks for the variables assigned in the make_dirs script, and if they're not there, redirect to that script...
+
 #User prompts will call the functions from the config file
 source make_meta.config
 
 #Title card
 figlet Automation!
-
-#Prompts for either identifying the Artwork File or creating one using the function defined earlier. Defines that path as "$ArtFile"
-cowsay -W 30 "Does the Artwork File exist? (Choose a number 1-3)"
-select ArtFile_option in "yes" "no" "quit"
-do
-	case $ArtFile_option in
-		yes) echo -e "\n*************************************************\n
-Input path to Artwork File. \nThe directory name should be: \n'Accession Number_Artwork Title' "
-		#Asks for user input, allows for tab completion of path
-			read -e ArtFile 
-			##Takes user input. might be ok with either a "/" or no "/"?? Is that possible?
-			#Assigns user input to the ArtFile variable
-			while [[ -z "$ArtFile" ]] ; do 
-					echo -e "\n*************************************************\n
-Input path to Artwork File. \nThe directory name should be: \n'Accession Number_Artwork Title' " &&
-read -e ArtFile
-				done
-			echo -e "\n*************************************************\n
-The Artwork File is $ArtFile"
-			break;;
-		no) MakeArtworkFile 
-			break;;
-		quit) exit 1 ;;
-	esac
-done
-
-#Prompts for either identifying the staging directory or creating one using the function defined earlier. Defines that path as "$SDir"
-cowsay "Does the Staging Directory exist? (Choose a number 1-3)"
-select SDir_option in "yes" "no" "quit"
-do
-	case $SDir_option in
-		yes) echo -e "\n*************************************************\n
-Input path to Staging Directory"
-		#Asks for user input, allows for tab completion of path
-			read -e SDir 
-			#Takes user input. might be ok with either a "/" or no "/"?? Is that possible?
-			#Confirms that the SDir variable is defined 
-			echo -e "\n*************************************************\n
-Staging Driectory is $SDir"
-			break;;
-		no) MakeStagingDirectory
-			break;;
-		quit) exit 1 ;;
-	esac
-done
-
-#Prompts user input for path to hard drive (or other carrier), defines that path as "$Volume"
-cowsay -p -W 31 "Input the path to the volume - Should begin with '/Volumes/' (use tab complete to help)"
-read -e Volume
-while [[ -z "$Volume" ]] ; do 
-	echo -e "Input the path to the volume - Should begin with '/Volumes/' (use tab complete to help)" && read -e Volume 
-done
-echo "The volume path is $Volume"
-
-#Uses the function defined at the top to copy media from the (now defined) variable Volume to the (now defined) variable Staging Directory
-cowsay -s "Copy all files from the volume to the staging directory?"
-select Copy_option in "yes" "no" "quit"
-do
-	case $Copy_option in
-		yes) CopyitVolumeStaging
-			break;;
-		no) break;;
-		quit) exit 1 ;;
-		esac
-	done
-
-#Going straight into disktype seemed weird, decided to add a prompt here...
-cowsay -b "Move on to metadata creation? Next step is to identify the device path to run disktype"
-select MoveOn_option in "yes" "quit"
-do
-	case $MoveOn_option in
-		yes) echo "Generating diskutil list..."
-			break;;
-		quit) exit 1 ;;
-	esac
-done
 
 #Prompts user input for path to hard drive (or other carrier), defines that path as "$Device"
 diskutil list
@@ -94,15 +22,18 @@ done
 echo "The device path is $Device"
 
 #Prompts user that disktype is being run? Not sure about this...
+#Prompts user that disktype is being run? Not sure about this...
 echo -e "\n*************************************************\n
 Run disktype on $Device (Choose a number 1-2)"
 select Disktype_option in "yes" "no"
 do
 	case $Disktype_option in
-		yes) disktype
+		yes) Run_disktype=1
+			#disktype
 			#Should run disktype on device path and then pipe output to tee, copy output to Staging Directory, Tech Specs dir in ArtFile and appendix in ArtFile
 			break;;
-		no) break;;
+		no) Run_disktype=0
+break;;
 	esac
 done  
 
@@ -112,10 +43,12 @@ Run tree on $Volume (Choose a number 1-2)"
 select Tree_option in "yes" "no"
 do
 	case $Tree_option in
-		yes) RunTree
+		yes) Run_tree=1
+#RunTree
 			#Runs tree function defined at the top of the doc. 
 			break;;
-		no) break;;
+		no) Run_tree=0
+break;;
 	esac
 done  
 
@@ -125,10 +58,12 @@ Run siegfried on $SDir (Choose a number 1-2)"
 select SF_option in "yes" "no"
 do
 	case $SF_option in
-		yes) RunSF
+		yes) Run_sf=1
+		#RunSF 
 			#Runs siegfried function defined at the top of the doc. 
 			break;;
-		no) break;;
+		no) Run_sf=0
+break;;
 	esac
 done  
 
@@ -138,10 +73,12 @@ Run MediaInfo on video files in $SDir (Choose a number 1-2)"
 select MI_option in "yes" "no"
 do
 	case $MI_option in
-		yes) RunMI
+		yes) Run_mediainfo=1
+#RunMI
 			#Runs mediainfo function defined at the top of the doc. 
 			break;;
-		no) break;;
+		no) Run_mediainfo=0
+break;;
 	esac
 done  
 
@@ -151,10 +88,12 @@ Create framemd5 text files for each of the video files in $SDir (Choose a number
 select Fmd5_option in "yes" "no"
 do
 	case $Fmd5_option in
-		yes) Make_Framemd5
+		yes) Run_framemd5=1
+#Make_Framemd5
 			#Runs ffmpeg to create 
 			break;;
-		no) break;;
+		no) Run_framemd5=0
+break;;
 	esac
 done  
 
@@ -164,11 +103,40 @@ Create QCTools reports for each of the video files in $SDir (Choose a number 1-2
 select QCT_option in "yes" "no"
 do
 	case $QCT_option in
-		yes) Make_QCT
+		yes) Run_QCTools=1
+		#Make_QCT
 			#Runs QCTools function defined at the top of the doc. 
 			break;;
-		no) break;;
+		no) Run_QCTools=0
+break;;
 	esac
 done
+
+#The following if statements will check the variable assignements from above to run the desired functions.
+#Next step is to change these functions in to bash scripts I think
+
+if [ $Run_disktype = 1 ] 
+then disktype
+fi
+
+if [ $Run_tree = 1 ]
+then RunTree
+fi
+
+if [ $Run_sf = 1 ]
+	then RunSF
+fi
+
+if [ $Run_mediainfo = 1 ]
+then RunMI
+fi
+
+if [ $Run_framemd5 = 1 ]
+	then Make_Framemd5
+fi
+
+if [ $Run_QCTools = 1 ]
+	then Make_QCT
+fi
 
 figlet Fin.  
