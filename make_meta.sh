f5385762693026f9597d2ebfc13dcaa2ceaed78a
eddycolloton <eddy.colloton@gmail.com>
2020-10-02 13:58:46 -0400

    Update make_meta.sh
    Added if statement to prompt user to use make_dirs.sh to assign ArtFile and SDir variables

--- make_meta.sh
+++ make_meta.sh
@@ -2,13 +2,34 @@
 
 #I'm trying to start breaking up the processes in the script, so this just the metadata tools that are run on the files
 #It requires inputs gathered from the make_dirs_config.sh though, so it's not really a stand a lone script. make_dirs has to be run first.
-#I should start this script with an if statement that checks for the variables assigned in the make_dirs script, and if they're not there, redirect to that script...
 
 #User prompts will call the functions from the config file
 source make_meta.config
 
-#Title card
-figlet Automation!
+#if statement that checks for the variables assigned in the make_dirs script, and if they're not there, redirect to that script?
+if [ -z "${ArtFile+x}" ]; 
+	then echo "The path to the Artwork File is not set! Run make_dirs.sh to assign an Artwork File?"
+	select No_ArtFile_option in "yes" "no" 
+	do
+		case $No_ArtFile_option in
+			yes) echo "Running make_dirs..." && source make_dirs.sh
+			break;;
+			no) echo "Quitting now..." && exit 1 ;; 
+			esac
+done; 
+else echo "ArtFile is set to '$ArtFile'"; fi
+
+if [ -z "${SDir+x}" ]; 
+	then echo "The path to the Staging Directory is not set! Run make_dirs.sh to assign SDir?"
+	select No_ArtFile_option in "yes" "no" 
+	do
+		case $No_ArtFile_option in
+			yes) echo "Running make_dirs..." && source make_dirs.sh
+			break;;
+			no) echo "Quitting now..." && exit 1 ;; 
+			esac
+done;
+else echo "Staging Directory is set to '$SDir'"; fi
 
 #Prompts user input for path to hard drive (or other carrier), defines that path as "$Device"
 diskutil list
@@ -21,7 +42,6 @@ Input the path to the device - Should be '/dev/disk#' " && read -e Device
 done
 echo "The device path is $Device"
 
-#Prompts user that disktype is being run? Not sure about this...
 #Prompts user that disktype is being run? Not sure about this...
 echo -e "\n*************************************************\n
 Run disktype on $Device (Choose a number 1-2)"
