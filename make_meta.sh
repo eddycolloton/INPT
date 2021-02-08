db79bb4533ae636cdfeb6ca9e51ab5f4b0d03eab
eddycolloton <eddy.colloton@gmail.com>
2021-02-08 14:04:54 -0700

    Update make_meta.sh
    removed multiple user inputs. Instead of the user saying "yes" or "no" to all of the metadata tools, they only say "yes" or "no" to disktype, and then everything else.
    
    Added Exiftool to the metadata tools with the function RunExif

--- make_meta.sh
+++ make_meta.sh
@@ -57,80 +57,18 @@ break;;
 	esac
 done  
 
-#Prompts user to run tree
+#Prompts user to run metadata tools
 echo -e "\n*************************************************\n
-Run tree on $Volume (Choose a number 1-2)"
-select Tree_option in "yes" "no"
+Run metadata tools (tree, siegfried, MediaInfo, Exiftool, framemd5, and qctools) on $Volume (Choose a number 1-2)"
+select Meta_option in "yes" "no"
 do
-	case $Tree_option in
-		yes) Run_tree=1
-#RunTree
-			#Runs tree function defined at the top of the doc. 
+	case $Meta_option in
+		yes) Run_meta=1
 			break;;
-		no) Run_tree=0
+		no) Run_meta=0
 break;;
 	esac
 done  
-
-#Prompts user to run siegfried file format id
-echo -e "\n*************************************************\n
-Run siegfried on $SDir (Choose a number 1-2)"
-select SF_option in "yes" "no"
-do
-	case $SF_option in
-		yes) Run_sf=1
-		#RunSF 
-			#Runs siegfried function defined at the top of the doc. 
-			break;;
-		no) Run_sf=0
-break;;
-	esac
-done  
-
-#Prompts user to run mediainfo 
-echo -e "\n*************************************************\n
-Run MediaInfo on video files in $SDir (Choose a number 1-2)"
-select MI_option in "yes" "no"
-do
-	case $MI_option in
-		yes) Run_mediainfo=1
-#RunMI
-			#Runs mediainfo function defined at the top of the doc. 
-			break;;
-		no) Run_mediainfo=0
-break;;
-	esac
-done  
-
-#Prompts user to make framemd5 text files for videos
-echo -e "\n*************************************************\n
-Create framemd5 text files for each of the video files in $SDir (Choose a number 1-2)"
-select Fmd5_option in "yes" "no"
-do
-	case $Fmd5_option in
-		yes) Run_framemd5=1
-#Make_Framemd5
-			#Runs ffmpeg to create 
-			break;;
-		no) Run_framemd5=0
-break;;
-	esac
-done  
-
-#Prompts user to make QCTools reports for ech video file in the staging directory
-echo -e "\n*************************************************\n
-Create QCTools reports for each of the video files in $SDir (Choose a number 1-2)"
-select QCT_option in "yes" "no"
-do
-	case $QCT_option in
-		yes) Run_QCTools=1
-		#Make_QCT
-			#Runs QCTools function defined at the top of the doc. 
-			break;;
-		no) Run_QCTools=0
-break;;
-	esac
-done
 
 #The following if statements will check the variable assignements from above to run the desired functions.
 #Next step is to change these functions in to bash scripts I think
@@ -139,24 +77,8 @@ if [ $Run_disktype = 1 ]
 then disktype
 fi
 
-if [ $Run_tree = 1 ]
-then RunTree
+if [ $Run_meta = 1 ] 
+then RunTree; RunSF; RunMI; RunExif; Make_Framemd5; Make_QCT 
 fi
-
-if [ $Run_sf = 1 ]
-	then RunSF
-fi
-
-if [ $Run_mediainfo = 1 ]
-then RunMI
-fi
-
-if [ $Run_framemd5 = 1 ]
-	then Make_Framemd5
-fi
-
-if [ $Run_QCTools = 1 ]
-	then Make_QCT
-fi
-
+ 
 figlet Fin.  
