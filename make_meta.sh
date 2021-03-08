8e0bf1b4434b26096c83de18f06ff329c3f1f25b
eddycolloton <eddy.colloton@gmail.com>
2021-03-08 14:54:59 -0700

    Update make_meta.sh
    Added "case" statements to allow the user to run each of the tools individually and added "MultiCopy=0" to the RunCopyit options to prevent inadvertently running MultiCopy

--- make_meta.sh
+++ make_meta.sh
@@ -17,7 +17,7 @@ if [ -z "${ArtFile+x}" ];
 			no) echo "Quitting now..." && exit 1 ;; 
 			esac
 done; 
-else echo "ArtFile is set to '$ArtFile'"; fi
+else echo "Artwork File is set to '$ArtFile'"; fi
 
 if [ -z "${SDir+x}" ]; 
 	then echo "The path to the Staging Directory is not set! Run make_dirs.sh to assign SDir?"
@@ -53,11 +53,11 @@ cowsay -s "Copy all files from the volume to the staging directory?"
 select Run_Copyit in "yes" "no, only certain directories" "none"
 do
 	case $Run_Copyit in
-		yes) Run_Copyit=1
+		yes) Run_Copyit=1 && Run_MultiCopy=0
 			break;;
 		"no, only certain directories") MultiSelect=1 && DeleteList && MultiSelection 
 			break;;
-		none) Run_Copyit=0
+		none) Run_Copyit=0 && Run_MultiCopy=0
 	esac
 done  
 
@@ -94,22 +94,10 @@ break;;
 	esac
 done  
 
-#Prompts user to tranfer files to Staging Driectory
-echo -e "\n*************************************************\n
-Copy all files from the volume to the staging directory?"
-select Run_Copyit in "yes" "no"
-do
-	case $Run_Copyit in
-		yes) Run_Copyit=1
-			break;;
-		no) Run_Copyit=0
-break;;
-	esac
-done  
-
 #Prompts user to run metadata tools
 echo -e "\n*************************************************\n
-This is the final prompt, applications will run after this response!
+If you select "yes," this will be the final prompt and applications will run after this response!\n
+Otherwise, you will be asked about each tool individually.
 \n*************************************************\n
 Run metadata tools (tree, siegfried, MediaInfo, Exiftool, framemd5, and qctools) on files copied to $SDir (Choose a number 1-2)"
 select Meta_option in "yes" "no"
@@ -120,7 +108,115 @@ do
 		no) Run_meta=0
 break;;
 	esac
+done
+
+if [[ $Run_meta=1 ]]; 
+	then echo -e "\n*************************************************\n
+Run tree on $Volume (Choose a number 1-2)"
+select Tree_option in "yes" "no"
+do
+	case $Tree_option in
+		yes) Run_tree=1
+#RunTree
+			#Runs tree function defined at the top of the doc. 
+			break;;
+		no) Run_tree=0
+break;;
+	esac
+done
+fi  
+
+
+if [[ $Run_meta=1 ]]; then
+#Prompts user to run siegfried file format id
+echo -e "\n*************************************************\n
+Run siegfried on $SDir (Choose a number 1-2)"
+select SF_option in "yes" "no"
+do
+	case $SF_option in
+		yes) Run_sf=1
+		#RunSF 
+			#Runs siegfried function defined at the top of the doc. 
+			break;;
+		no) Run_sf=0
+break;;
+	esac
 done  
+fi
+
+
+if [[ $Run_meta=1 ]]; then
+#Prompts user to run mediainfo 
+echo -e "\n*************************************************\n
+Run MediaInfo on video files in $SDir (Choose a number 1-2)"
+select MI_option in "yes" "no"
+do
+	case $MI_option in
+		yes) Run_mediainfo=1
+#RunMI
+			#Runs mediainfo function defined at the top of the doc. 
+			break;;
+		no) Run_mediainfo=0
+break;;
+	esac
+done
+fi
+
+
+if [[ $Run_meta=1 ]]; then
+#Prompts user to run exiftool
+echo -e "\n*************************************************\n
+Run Exiftool on image files in $SDir (Choose a number 1-2)"
+select Exif_option in "yes" "no"
+do
+	case $Exif_option in
+		yes) Run_exif=1
+#RunMI
+			#Runs mediainfo function defined at the top of the doc. 
+			break;;
+		no) Run_exif=0
+break;;
+	esac
+done    
+fi
+
+if [[ $Run_meta=1 ]]; then
+#Prompts user to make framemd5 text files for videos
+echo -e "\n*************************************************\n
+Create framemd5 text files for each of the video files in $SDir (Choose a number 1-2)"
+select Fmd5_option in "yes" "no"
+do
+	case $Fmd5_option in
+		yes) Run_framemd5=1
+#Make_Framemd5
+			#Runs ffmpeg to create 
+			break;;
+		no) Run_framemd5=0
+break;;
+	esac
+done  
+fi
+
+if [[ $Run_meta=1 ]]; then
+#Prompts user to make QCTools reports for ech video file in the staging directory
+echo -e "
+\n*************************************************\n
+This will be the final prompt and applications will run after this response!\n
+Create QCTools reports for each of the video files in $SDir (Choose a number 1-2)
+\n*************************************************\n"
+select QCT_option in "yes" "no"
+do
+	case $QCT_option in
+		yes) Run_QCTools=1
+		#Make_QCT
+			#Runs QCTools function defined at the top of the doc. 
+			break;;
+		no) Run_QCTools=0
+break;;
+	esac
+done
+fi
+
 
 #The following if statements will check the variable assignements from above to run the desired functions.
 #Next step is to change these functions in to bash scripts I think
@@ -141,4 +237,29 @@ if [ $Run_meta = 1 ]
 then RunTree; RunSF; RunMI; RunExif; Make_Framemd5; Make_QCT 
 fi
  
+
+if [ $Run_tree = 1 ]
+then RunTree
+fi
+
+if [ $Run_sf = 1 ]
+	then RunSF
+fi
+
+if [ $Run_mediainfo = 1 ]
+then RunMI
+fi
+
+if [ $Run_exif = 1 ]
+then RunExif
+fi
+
+if [ $Run_framemd5 = 1 ]
+	then Make_Framemd5
+fi
+
+if [ $Run_QCTools = 1 ]
+	then Make_QCT
+fi
+
 figlet Fin. 
