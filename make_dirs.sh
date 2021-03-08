beea0b8e5f07a887b0346185cb34c978e9640867
eddycolloton <eddy.colloton@gmail.com>
2021-03-08 13:04:57 -0700

    Update make_dirs.sh
    Updated process for making artwork folder and staging directory and moved up setting the "device path"

--- make_dirs.sh
+++ make_dirs.sh
@@ -4,50 +4,82 @@ source HMSG_auto/HMSG_auto.config
 
 figlet Automation!
 
-#Prompts for either identifying the Artwork File or creating one using the function defined earlier. Defines that path as "$ArtFile"
-cowsay -W 30 "Does the Artwork File exist? (Choose a number 1-3)"
-select ArtFile_option in "yes" "no" "quit"
-do
-	case $ArtFile_option in
-		yes) echo -e "\n*************************************************\n
-Input path to Artwork File! \nNavigate to the Artwork Files on the T:/ drive in Finder. 
-Next, find the Artwork File, and click through to the directory named: \n'Accession Number_Artwork Title' 
-(it is often inside a directory named 'time-based media')
-'Drag and drop' the 'Accession Number_Artwork Title' directory into terminal"
-			while [[ -z "$ArtFile" ]] ; do 
-			read -e ArtFile
-#Asks for user input, allows for tab completion of path. might be ok with either a "/" or no "/"?? 
-#Assigns user input to the ArtFile variable
-		done
-			echo -e "\n*************************************************\n
-The Artwork File is $ArtFile"
-			break;;
-		no) MakeArtworkFile 
-			break;;
-		quit) exit 1 ;;
-	esac
-done
 
+echo -e "\n*************************************************\n
+Input artist's first name"
+	read -e ArtistFirstName
+	#Asks for user input and assigns it to variable
+echo -e "\n*************************************************\n
+Input artist's last name"
+	read -e ArtistLastName
+	#Asks for user input and assigns it to variable
+	echo -e "\n Artist name is $ArtistFirstName $ArtistLastName"
+cowsay -W 30 "Enter a number to set the path to the Artwork File on the T:\ drive:"
+#prompt for select command
+IFS=$'\n'; select artdir in $(find /path/to/artworkfiles/ -type d -iname "*$ArtistLastName*") "Input path" "Create Artwork File" ; do
+#lists options for select command - the IFS statment stops it from escaping when it hits spaces in directory names
+  	if [[ $artdir = "Input path" ]]
+  	then while [[ -z "$ArtFile" ]] ; do 
+		read -e ArtFile
+		#Asks for user input and assigns it to variable
+		FindAccessionNumber
+		#searches the Artwork File for the accession number, and assigns it to the $accession variable
+  		done
+  	elif [[ $artdir = "Create Artwork File" ]]
+  	then MakeArtworkFile 
+	else
+		ArtFile=$artdir
+		#assigns variable to the users selection from the select menu
+		#NEED TO CHANGE! THIS ASSIGNS ART FILE TO PARENT DIRECTORY, SHOULD BE "title_accession" DIR!
+		echo -e "\n*************************************************\n\nThe Artwork File is $ArtFile\n"
+		FindAccessionNumber
+		#searches the Artwork File for the accession number, and assigns it to the $accession variable
+	fi
+break			
+done;
+
+cowsay "Enter a number to set the path to the Staging Directory on the TBMA DroBo:"
 #Prompts for either identifying the staging directory or creating one using the function defined earlier. Defines that path as "$SDir"
-cowsay "Does the Staging Directory exist? (Choose a number 1-3)"
-select SDir_option in "yes" "no" "quit"
-do
-	case $SDir_option in
-		yes) echo -e "\n*************************************************\n
-Input path to Staging Directory on TBMA Drobo
-It should be named 'Accession-Number_Artist's Last Name'"
-		#Asks for user input, allows for tab completion of path
-			read -e SDir 
-			#Takes user input. might be ok with either a "/" or no "/"?? Is that possible?
-			#Confirms that the SDir variable is defined 
-			echo -e "\n*************************************************\n
-Staging Driectory is $SDir"
+IFS=$'\n'; select SDir_option in $(find /Volumes/TBMA\ Drobo/Time\ Based\ Media\ Artwork/ -type d -iname "*$ArtistLastName*") "Input path" "Create Staging Directory" ; do
+	if [[ $SDir_option = "Input path" ]]
+  	then while [[ -z "$SDir" ]] ; do 
+		read -e SDir 
+		#Takes user input. might be ok with either a "/" or no "/"?? Is that possible?
+		echo -e "\n*************************************************\n\n
+Staging Driectory is $SDir \n\n*************************************************\n"
+#Confirms that the SDir variable is defined 
+		done
+	elif [[ $SDir_option = "Create Staging Directory" ]]
+  	then MakeStagingDirectory
+	#Runs MakeStagingDirectory function defined in make_meta.config
+	else
+		SDir=$SDir_option
+		#assigns variable to the users selection from the select menu
+		echo -e "\n*************************************************\n\nthe Staging Directory is $SDir \n\n*************************************************\n"
+	fi
+break			
+done;
+
+echo -e "Show diskutil list? \n"
+select dlist in "Yes, show diskutil list" "No, I already know the device path"
+	do
+		case $dlist in
+			"Yes, show diskutil list") 
+				 echo "Generating list..."
+				 diskutil list
 			break;;
-		no) MakeStagingDirectory
+			"No, I already know the device path") echo "Moving on..."
 			break;;
-		quit) exit 1 ;;
-	esac
+		esac
+	done
+
+#Prompts user input for path to hard drive (or other carrier), defines that path as "$Device"
+#diskutil list
+while [[ -z "$Device" ]] ; do
+	cowsay -p -W 31 "Input the path to the device - Should be '/dev/disk#' " 
+	read -e Device
 done
+echo -e "The device path is $Device \n"
 
 #Prompts user input for path to hard drive (or other carrier), defines that path as "$Volume"
 cowsay -p -W 31 "Input the path to the volume - Should begin with '/Volumes/' (use tab complete to help)"
@@ -57,13 +89,4 @@ while [[ -z "$Volume" ]] ; do
 done
 echo "The volume path is $Volume"
 
-#Going straight into disktype seemed weird, decided to add a prompt here...
-cowsay -b "Move on to metadata creation? Next step is to identify the device path to run disktype"
-select MoveOn_option in "yes" "quit"
-do
-	case $MoveOn_option in
-		yes) echo "Generating diskutil list..." && source make_meta.sh
-			break;;
-		quit) exit 1 ;;
-	esac
-done
+source make_meta_local.sh
