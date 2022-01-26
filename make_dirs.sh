f933a2a9bd92aae7fa9c99b9317188657830e403
eddycolloton <eddy.colloton@gmail.com>
2022-01-26 11:41:38 -0700

    Update make_dirs.sh
    A user dragged and dropped a volume into terminal to input the name, and the trailing space on the end of the input was interpreted later in the script as a "\" which broke some of the CLI tools called in make_meta.sh. I've added a sed command after the "read" volume input to clear any spaces from the input.

--- make_dirs.sh
+++ make_dirs.sh
@@ -87,7 +87,10 @@ echo -e "The device path is $Device \n"
 
 #Prompts user input for path to hard drive (or other carrier), defines that path as "$Volume"
 cowsay -p -W 31 "Input the path to the volume - Should begin with '/Volumes/' (use tab complete to help)"
-read -e Volume
+read -e VolumeInput
+Volume="$(echo -e "${VolumeInput}" | sed -e 's/[[:space:]]*$//')"
+#If the volume name is dragged and dropped into terminal, the trail whitespace can eventually be interpreted as a "\" which breaks the CLI tools called in make_meta.sh. To prevent this, the sed command above is used.
+#I find sed super confusing, I lifted this command from https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
 while [[ -z "$Volume" ]] ; do 
 	echo -e "Input the path to the volume - Should begin with '/Volumes/' (use tab complete to help)" && read -e Volume 
 done
