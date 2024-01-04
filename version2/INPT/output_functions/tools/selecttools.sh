366923cceeb74e997f48e4fad6646e3de7d606f8
eddycolloton <eddy.colloton@gmail.com>
2024-01-03 17:20:16 -0700

    reworked selecttools.sh, broke large function down into smaller functions, removed 'while' loops and run 'go back a step' with if statements calling smaller functions instead.

--- version2/INPT/output_functions/tools/selecttools.sh
+++ version2/INPT/output_functions/tools/selecttools.sh
@@ -1,19 +1,7 @@
-function SelectTools {
-	# Prompts user to run metadata tools
-	echo -e "\n*************************************************\n
-If you select \"yes,\" this will be the final prompt and applications will run after this response!\n
-Otherwise, you will be asked about each tool individually.
-\n*************************************************\n"
-	sleep 1
+#!/bin/bash
 
-#Consider - changing all tool selection to a function that takes 2 vars, option and repeat_prev_option
-#Then run functions and repeat functions w/ if statement or something instead of while loop
-
-	meta_option_again=yes
-	while [[ "$meta_option_again" = yes ]]
-	# https://unix.stackexchange.com/questions/232761/get-script-to-run-again-if-input-is-yes
-	do
-	echo -e "Run metadata tools (tree, siegfried, MediaInfo, Exiftool, framemd5, and qctools) on files copied to $SDir (Choose a number 1-2)"
+function SelectMeta {
+	echo -e "\nRun metadata tools (tree, siegfried, MediaInfo, Exiftool, framemd5, and qctools) on files copied to $SDir (Choose a number 1-2)"
 	select Meta_option in "yes" "no"; do
 		case $Meta_option in
 			yes)
@@ -40,129 +28,157 @@ Otherwise, you will be asked about each tool individually.
 			esac
 		done
 	fi
+}
 
-	if [[ "$Run_meta" -eq "0" ]]; then
-		echo -e "\n*************************************************\nRun tree on $Volume (Choose a number 1-2)"
-		select Tree_option in "yes" "no" "go back a step"; do
-			case $Tree_option in
-				yes)
-					Run_tree=1
-					tree_option_again=no
-					break;;
-				no)
-					Run_tree=0
-					tree_option_again=no
-					break;;
-				"go back a step")
-					unset Meta_option Run_meta Tree_option
-					meta_option_again=yes
-					break;;
-			esac
-		done
-	fi
-
-	if [[ "$meta_option_again" = yes ]]; then 
-		echo -e "\n\n!! Going back a step !! \n"
-	fi
+SelectOption () {
+    local option_select="$1"
+    local option_again="$2"
+    local prev_option_again="$3"
+	select options in "yes" "no" "go back a step"; do
+		case $options in
+			yes)
+				select_value=1
+				select_again=no
+                eval "$option_select=\$select_value"
+                eval "$option_again=\$select_again"
+				break;;
+			no)
+				select_value=0
+				select_again=no
+                eval "$option_select=$select_value"
+                eval "$option_again=$select_again"
+				break;;
+			"go back a step")
+                select_prev_again=yes
+                eval "$prev_option_again=\$select_prev_again"
+				break;;
+		esac
 	done
+}
+
+function SelectTree {
+	echo -e "\n*************************************************\nRun tree on $Volume (Choose a number 1-2)"
+	SelectOption "Run_tree" "tree_option_again" "meta_option_again"
+}
+
+function SelectSF {
+	echo -e "\n*************************************************\nRun siegfried on $SDir (Choose a number 1-2)"
+	SelectOption "Run_sf" "sf_option_again" "tree_option_again"
+}
+
+function SelectMI {
+	echo -e "\n*************************************************\nRun MediaInfo on video files in $SDir (Choose a number 1-2)"
+	SelectOption "Run_mediainfo" "mi_option_again" "sf_option_again"
+}
+
+function SelectExif {
+	echo -e "\n*************************************************\nRun Exiftool on image files in $SDir (Choose a number 1-2)"
+	SelectOption "Run_exif" "exif_option_again" "mi_option_again"
+}
+
+function SelectFmd5 {
+	echo -e "\n*************************************************\nCreate framemd5 text files for each of the video files in $SDir (Choose a number 1-2)"
+	SelectOption "Run_framemd5" "Fmd5_option_again" "exif_option_again"
+}
+
+function SelectQCT {
+	echo -e "\n*************************************************\nThis will be the final prompt, and applications will run after this response!\nCreate QCTools reports for each of the video files in $SDir (Choose a number 1-2)\n*************************************************\n"
+	SelectOption "Run_QCTools" "QCT_option_again" "Fmd5_option_again"
+}
+
+function SelectTools {
+	# Prompts user to run metadata tools
+	echo -e "\n*************************************************\n
+If you select \"yes,\" this will be the final prompt and applications will run after this response!\n
+Otherwise, you will be asked about each tool individually.
+\n*************************************************"
+	sleep 1
+
+	meta_option_again=yes
+	SelectMeta
 
 	if [[ "$Run_meta" -eq "0" ]]; then
-		echo -e "\n*************************************************\nRun siegfried on $SDir (Choose a number 1-2)"
-		select SF_option in "yes" "no" "go back a step"; do
-			case $SF_option in
-				yes)
-					Run_sf=1
-					sf_option_again=no
-					break;;
-				no)
-					Run_sf=0
-					sf_option_again=no
-					break;;
-				"go back a step")
-					unset Tree_option Run_tree SF_option
-					tree_option_again=yes
-					break;;
-			esac
-		done
+		tree_option_again=yes
+		SelectTree
+	fi
+
+	if [[ "$meta_option_again" = "yes" ]]; then 
+		unset Run_meta
+		SelectMeta
+		SelectTree
+	fi
+	
+	if [[ "$Run_meta" -eq "0" ]]; then
+		sf_option_again=yes
+		SelectSF
+	fi
+
+	if [[ "$tree_option_again" = "yes" ]]; then
+		unset Run_tree
+		SelectTree
+		SelectSF
 	fi
 
 	if [[ "$Run_meta" -eq "0" ]]; then
-		echo -e "\n*************************************************\nRun MediaInfo on video files in $SDir (Choose a number 1-2)"
-		select MI_option in "yes" "no" "go back a step"; do
-			case $MI_option in
-				yes)
-					Run_mediainfo=1
-					mi_option_again=no
-					break;;
-				no)
-					Run_mediainfo=0
-					mi_option_again=no
-					break;;
-				"go back a step")
-					unset SF_option Run_sf MI_option
-					sf_option_again=yes
-					break;;
-			esac
-		done
+		mi_option_again=yes
+		SelectMI
+	fi
+
+	if [[ "$sf_option_again" = "yes" ]]; then
+		unset Run_sf
+		SelectSF
+		SelectMI
 	fi
 
 	if [[ "$Run_meta" -eq "0" ]]; then
-		echo -e "\n*************************************************\nRun Exiftool on image files in $SDir (Choose a number 1-2)"
-		select Exif_option in "yes" "no" "go back a step"; do
-			case $Exif_option in
-				yes)
-					Run_exif=1
-					exif_option_again=no
-					break;;
-				no)
-					Run_exif=0
-					exif_option_again=no
-					break;;
-				"go back a step")
-					unset MI_option Run_mediainfo Exif_option
-					mi_option_again=yes
-					break;;
-			esac
-		done
+		exif_option_again=yes
+		SelectExif
+	fi
+	
+	if [[ "$mi_option_again" = "yes" ]]; then
+		unset Run_mediainfo
+		SelectMI
+		SelectExif
 	fi
 
 	if [[ "$Run_meta" -eq "0" ]]; then
-		echo -e "\n*************************************************\nCreate framemd5 text files for each of the video files in $SDir (Choose a number 1-2)"
-		select Fmd5_option in "yes" "no" "go back a step"; do
-			case $Fmd5_option in
-				yes)
-					Run_framemd5=1
-					Fmd5_option_again=no
-					break;;
-				no)
-					Run_framemd5=0
-					Fmd5_option_again=no
-					break;;
-				"go back a step")
-					unset Exif_option Run_exif Fmd5_option
-					exif_option_again=yes
-					break;;
-			esac
-		done
+		Fmd5_option_again=yes
+		SelectFmd5
+	fi
+	
+	if [[ "$exif_option_again" = "yes" ]]; then
+		unset Run_exif
+		SelectExif
+		SelectFmd5
 	fi
 
 	if [[ "$Run_meta" -eq "0" ]]; then
-		echo -e "\n*************************************************\nThis will be the final prompt, and applications will run after this response!\nCreate QCTools reports for each of the video files in $SDir (Choose a number 1-2)\n*************************************************\n"
-		select QCT_option in "yes" "no"; do
-			case $QCT_option in
+		QCT_option_again=yes
+		SelectQCT
+	fi
+
+	if [[ "$Fmd5_option_again" = "yes" ]]; then
+		unset Run_framemd5
+		SelectFmd5
+		SelectQCT
+	fi
+
+	if [[ "$Run_QCTools" -eq "1" ]]; then
+		echo -e "\nConfirm you would like to create QCTools reports for each of the video files in $SDir:"
+		select QCT_confirm in "yes" "no"; do
+			case $QCT_confirm in
 				yes)
-					Run_QCTools=1
 					QCT_option_again=no
 					break;;
 				no)
-					Run_QCTools=0
-					QCT_option_again=no
-					break;;
-				"go back a step")
-					unset Fmd5_option Run_framemd5 QCT_option
-					Fmd5_option_again=yes
+					QCT_option_again=yes
 					break;;
 			esac
 		done
+	fi
+
+	if [[ "$QCT_option_again" = "yes" ]]; then
+		unset Run_QCTools
+		SelectQCT
 	fi
 }
